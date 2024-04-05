dbea1ff6e8269f2baee0c14e924f0a5f7df58ff6
rupomhasan <rupom.hasan607299@gmail.com>
2024-04-05 22:54:57 +0600

    Completed coding journey for HomeCare Hub: refactored operations, finalized codebase. Enhancements boost readability, maintainability, and functionality.

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require("express");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const app = express();
-const port = process.env.PORT || 25000;
+const port = process.env.PORT || 33000;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 //Use  MiddleWare
 app.use(
   cors({
-    origin: [""],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
@@ -51,8 +51,8 @@ const verify = (req, res, next) => {
 
 async function run() {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
+     client.connect();
+     client.db("admin").command({ ping: 1 });
 
     //Auth connection
 
@@ -87,7 +87,8 @@ async function run() {
     const choseUsCollection = dataBaseName.collection("choseUs");
     const reviewCollection = dataBaseName.collection("review");
     const usersCollection = dataBaseName.collection("users");
-
+    const sponsorCollection = dataBaseName.collection("sponsor");
+    const teamCollection = dataBaseName.collection("team");
     // All Get Operation
 
     app.get("/", (req, res) => {
@@ -107,16 +108,11 @@ async function run() {
     // done : http://localhost:5000/api/v1/services?service_name=tree_surgeon
     // done : http://localhost:5000/api/v1/services?page=1&limit=10
     // done : http://localhost:5000/api/v1/services?minPrice=6000&maxPrice=7000
-    app.get("/api/v1/services", verify, async (req, res) => {
+    app.get("/api/v1/services", async (req, res) => {
       try {
-        const decodedEmail = req.email;
-        const user = req.body;
-        console.log(user, decodedEmail);
-        if (decodedEmail.email !== user.email) {
-          return res.status(403).send({ message: "Forbidden Access" });
-        }
-
         const queryObj = {};
+        const email = req.query.email;
+        const offer = req.query.Offer;
         const minPrice = parseFloat(req.query.minPrice);
         const maxPrice = parseFloat(req.query.maxPrice);
         const limit = Number(req.query.limit);
@@ -124,6 +120,22 @@ async function run() {
         const skip = (page - 1) * limit;
         let serviceName = req.query.service_name;
         const category = req.query.category;
+        if (email) {
+          const myService = await serviceCollection
+            .find({ "Service_Provider.Email": email })
+            .toArray();
+          res.send(myService);
+          return;
+        }
+
+        if (offer) {
+          const query = { Offer: { $exists: true } };
+          const result = await serviceCollection
+            .find(query)
+            .limit(limit)
+            .toArray();
+          return res.send(result);
+        }
 
         if (minPrice && maxPrice) {
           queryObj.Service_Price = { $gte: minPrice, $lte: maxPrice };
@@ -151,14 +163,60 @@ async function run() {
         res.status(500).send({ status: 500, message: "Internal server Error" });
       }
     });
-    app.get("/api/v1/service/:id", verify, async (req, res) => {
+
+    app.get("/api/v1/offer/count", async (req, res) => {
+      const offer = req.query.Offer;
+      if (offer) {
+        const query = { Offer: { $exists: true } };
+        const result = await serviceCollection.countDocuments(query);
+        res.send({ result });
+      }
+    });
+    app.get("/api/v1/service/count", async (req, res) => {
+      const offer = req.query.Offer;
+      const result = await serviceCollection.countDocuments(offer);
+      res.send({ totalServices: result });
+    });
+    app.get("/api/v1/booking/count", verify, async (req, res) => {
       try {
         const decodedEmail = req.email;
-        const user = req.body;
-        console.log(user, decodedEmail);
-        if (decodedEmail.email !== user.email) {
+        const customerEmail = req.query.email;
+        if (decodedEmail.email !== customerEmail) {
           return res.status(403).send({ message: "Forbidden Access" });
         }
+
+        const result = await bookingCollection.countDocuments({
+          "customer.email": customerEmail,
+        });
+
+        result === 0 ? res.json({ booking: 0 }) : res.json({ booking: result });
+      } catch (error) {
+        res.status(500).send({ status: 500, message: "Internal Server Error" });
+      }
+    });
+    app.get("/api/v1/employee", verify, async (req, res) => {
+      try {
+        const decodedEmail = req.email;
+        const email = req.query.email;
+
+        if (decodedEmail.email !== email) {
+          return res.status(403).send({ message: "Forbidden Access" });
+        }
+
+        const result = await teamCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ status: 500, message: "Internal Server Error" });
+      }
+    });
+    app.get("/api/v1/service/:id", async (req, res) => {
+      try {
+        // const decodedEmail = req.email;
+        // const user = req.body;
+        // console.log(user, decodedEmail);
+        // if (decodedEmail.email !== user.email) {
+        //   return res.status(403).send({ message: "Forbidden Access" });
+        // }
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
         const result = await serviceCollection.findOne(query);
@@ -197,6 +255,32 @@ async function run() {
         res.status(500).send({ status: 500, message: "Internal Server Error" });
       }
     });
+    app.get("/api/v1/customer/bookings", verify, async (req, res) => {
+      try {
+        const queryObj = {};
+        const decodedEmail = req.email;
+        const email = req.query.email;
+
+        if (decodedEmail.email !== email) {
+          return res.status(403).send({ message: "Forbidden Access" });
+        }
+        if (email) {
+          queryObj["provider.Email"] = email;
+        }
+        const result = await bookingCollection.find(queryObj).toArray();
+        res.send(result);
+      } catch (err) {
+        res.status(500).send({ status: 500, message: "Internal Server Error" });
+      }
+    });
+    app.get("/api/v1/sponsor", async (req, res) => {
+      try {
+        const result = await sponsorCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ status: 500, message: "Internal Server Error" });
+      }
+    });
     app.get("/api/v1/user/review", async (req, res) => {
       try {
         const result = await reviewCollection.find().toArray();
@@ -205,24 +289,7 @@ async function run() {
         res.send({ status: 500, message: "Internal server error" });
       }
     });
-    app.get("/api/v1/users", verify, async (req, res) => {
-      try {
-        let queryObj = {};
-        const email = req.query.email;
-        const decodedEmail = req.email;
-
-        if (decodedEmail.email !== email) {
-          return res.status(403).send({ message: "Forbidden" });
-        }
-        if (email) {
-          queryObj.email = email;
-        }
-        const result = await usersCollection.find(queryObj).toArray();
-        res.send(result);
-      } catch (err) {
-        res.status(500).send({ status: 500, message: "Internal Server Error" });
-      }
-    });
+   
 
     app.get("/api/v1/choseUs", async (req, res) => {
       try {
@@ -234,9 +301,16 @@ async function run() {
     });
     // All the post operation
 
-    app.post("/api/v1/create-service", async (req, res) => {
+    app.post("/api/v1/create-service", verify, async (req, res) => {
       try {
+        const email = req.query.email;
+        const decodedEmail = req.email;
+
+        if (decodedEmail.email !== email) {
+          return res.status(403).send({ message: "Forbidden" });
+        }
         const data = req.body;
+        console.log(data);
         const result = await serviceCollection.insertOne(data);
         res.send(result);
       } catch (err) {
@@ -252,7 +326,6 @@ async function run() {
         if (decodedEmail.email !== email) {
           return res.status(403).send({ message: "Forbidden Access" });
         }
-        console.log(decodedEmail, email);
 
         const result = await bookingCollection.insertOne(data);
         res.send(result);
@@ -280,29 +353,30 @@ async function run() {
       }
     });
     //all update operation
-
-    app.patch("/api/v1/update-user", async (req, res) => {
+    app.put("/api/v1/update/booking/:id", verify, async (req, res) => {
       try {
+        const id = req.params.id;
         const email = req.query.email;
-        const queryObj = { email: email };
-        const user = req.body;
-        const updatedUser = {
+        const decodedEmail = req.email;
+        const data = req.body.status.toLowerCase();
+        console.log(email, data);
+
+        if (email !== decodedEmail.email) {
+          return res.status(403).send({ message: "Forbidden Access" });
+        }
+        const query = { _id: new ObjectId(id) };
+        const update = {
           $set: {
-            username: user?.username,
-            email: user?.email,
-            password: user?.password,
-            fullName: user?.fullName,
-            age: user?.age,
-            address: user?.address,
-            phoneNumber: user?.phoneNumber,
+            "customer.status": data,
           },
         };
-        const result = await usersCollection.updateOne(queryObj, updatedUser);
+        const result = await bookingCollection.updateOne(query, update);
         res.send(result);
-      } catch (err) {
+      } catch (error) {
         res.status(500).send({ status: 500, message: "Internal server error" });
       }
     });
+
 
     app.patch("/api/v1/update-service/:id", async (req, res) => {
       try {
@@ -339,23 +413,7 @@ async function run() {
       }
     });
     // all delete operation
-    app.delete("/api/v1/clear-user/:id", verify, async (req, res) => {
-      try {
-        const id = req.params.id;
-        const email = req.query.email;
-        const decodedEmail = req.email;
-
-        if (email !== decodedEmail.email) {
-          return res.status(403).send({ message: "Forbidden Access" });
-        }
-        const query = { _id: new ObjectId(id) };
-        const result = await usersCollection.deleteOne(query);
-        res.send(result);
-      } catch (err) {
-        console.log(err);
-        res.status(500).send({ status: 500, message: "Internal server error" });
-      }
-    });
+   
 
     app.delete("/api/v1/clear-service/:id", verify, async (req, res) => {
       try {
@@ -375,6 +433,21 @@ async function run() {
         res.status(500).send({ status: 500, message: "Internal server error" });
       }
     });
+    app.delete("/api/v1/clear-allServices", verify, async (req, res) => {
+      try {
+        const email = req.query.email;
+        const decodedEmail = req.email;
+        if (decodedEmail.email !== email) {
+          return res.status(403).send({ message: "Forbidden Access" });
+        }
+        const result = await serviceCollection.deleteMany({
+          "Service_Provider.Email": email,
+        });
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ status: 500, message: "Internal server error" });
+      }
+    });
     app.delete("/api/v1/user/clear-booking/:id", verify, async (req, res) => {
       try {
         const id = req.params.id;
@@ -390,6 +463,17 @@ async function run() {
         console.log(err);
         res.status(500).send({ status: 500, message: "Internal server error" });
       }
+    });
+    app.delete("/api/v1/user/clear-allBooking", verify, async (req, res) => {
+      const email = req.query.email;
+      const decodedEmail = req.email;
+      if (decodedEmail.email !== email) {
+        return res.status(403).send({ message: "Forbidden Access" });
+      }
+      const result = await bookingCollection.deleteMany({
+        "customer.email": email,
+      });
+      res.send(result);
     });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
